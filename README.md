96a2bafbac31a9939aa919798496ca792fb439dd
Marc <marckiuy@gmail.com>
2021-12-17 02:33:13 +0000

    V0.6 Added distro option and some placeholders

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 <h1 align="center">Welcome to hashcloud 👋</h1>
 <p>
-  <img alt="Version" src="https://img.shields.io/badge/version-POC-blue.svg?cacheSeconds=2592000" />
+  <img alt="Version" src="https://img.shields.io/badge/version-0.6-blue.svg?cacheSeconds=2592000" />
   <a href="https://github.com/theirsecurity/hashcloud" target="_blank">
     <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
   </a>
 </p>
 
-> This script is an initial POC to set up a cloud server automatically with Hashcat, Seclist and OpenCL drivers to enable hashcat to run on the cloud with minimal user set up. I have tested this on Vultr's hosting platform and evidence of this can be seen either on my blog or youtube.
+> This script sets up a cloud server automatically with Hashcat, Seclist and OpenCL drivers to enable hashcat to run on the cloud with minimal user set up. I have tested this on Vultr's hosting platform and evidence of this can be seen either on my blog or youtube. This version adds support for 4 different linux distributions. GPU support is mentioned in the code which will be added in a future release.
 
 ### 🏠 [Homepage](https://github.com/theirsecurity/hashcloud)
 
@@ -23,6 +23,15 @@ Identify the script that applies to your cloud server then use
 chmod +x BASH_SCRIPT_NAME
 ./BASH_SCRIPT_NAME
 
+This will bring up the help section
+
+To set which linux distribution you will be installing on use one of the below
+
+./hashcat.sh -o deb #This is for any debian based distros
+./hashcat.sh -o fedora
+./hashcat.sh -o arch
+./hashcat.sh -o ubuntu 
+
 Once this has run you need to copy over the hash file you want to use with hascat, i did this using SCP to the /root/hashes folder e.g
 scp /INSERT_LOCAL_HASH_DIRECTORY CLOUD_USERNAME@CLOUD_IP:/hashes
 ```
@@ -33,4 +42,3 @@ scp /INSERT_LOCAL_HASH_DIRECTORY CLOUD_USERNAME@CLOUD_IP:/hashes
 
 * Website: https://theirsecurity.com
 * Github: [@theirsecurity](https://github.com/theirsecurity)
-
---
+++ hashcloud.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+#Test Script + Root Check + All versions
+# Alma, Arch, Cent, Debian, Fedora, Rocky, Ubuntu, VzLinux
+
+#Vars
+OS=""
+GRAPHICS=""
+VERS="0.60"
+
+#border function
+border()
+{
+    title="| $1 |"
+    edge=$(echo "$title" | sed 's/./-/g')
+    echo "$edge"
+    echo "$title"
+    echo "$edge"
+}
+
+#Help and Welcome
+usage() {
+  echo "██   ██  █████  ███████ ██   ██  ██████ ██       ██████  ██    ██ ██████  "
+  echo "██   ██ ██   ██ ██      ██   ██ ██      ██      ██    ██ ██    ██ ██   ██ "
+  echo "███████ ███████ ███████ ███████ ██      ██      ██    ██ ██    ██ ██   ██ "
+  echo "██   ██ ██   ██      ██ ██   ██ ██      ██      ██    ██ ██    ██ ██   ██ "
+  echo "██   ██ ██   ██ ███████ ██   ██  ██████ ███████  ██████   ██████  ██████  "
+  echo ""
+  echo "Version:        $VERS"
+  echo "Created By:     marc@theirsecurity.com"
+  echo "Github Release: http://github.com/theirsecurity/hashcloud"
+  border "Supported OS"
+  echo "To select which OS you want to use run ./hashcloud -o XXX"
+  echo "Debian Linux  -o deb"
+  echo "Arch Linux    -o arch"
+  echo "Fedora Linux  -o fedora"
+  echo "Ubuntu        -o ubuntu"
+  echo ""
+  border "Supported GPU"
+  echo "GPU support will be included in future release"
+  echo ""
+}
+exit_abnormal() {
+  usage
+  exit 1
+}
+
+#Get opts
+while getopts ":o:h:g:" options; do
+  case "${options}" in
+    o)
+     OS=${OPTARG}
+     ;;
+    :)
+    echo "ERROR: -${OPTARG} requires an argument"
+    exit_abnormal
+    ;;
+    h)
+    exit_abnormal
+    ;;
+    g)
+    GRAPHICS=${OPTARG}
+    ;;
+  esac
+done
+
+#Check if a valid os Selected
+if [[ $OS = "deb" ]] || [[ $OS = "arch" ]] || [[ $OS = "ubuntu" ]] || [[ $OS = "fedora" ]]
+then
+  echo ""
+else
+  border "ERROR:Invalid OS Selected"
+  exit_abnormal
+fi
+
+#If options selected move to root check
+if [[ $EUID -ne 0 ]]; then
+  border "ERROR: This script needs to run as root for package installation"
+  exit_abnormal
+fi
+
+#check GPU option
+if [[ $GRAPHICS = "n" ]]
+then
+  echo "Nvidia GPU Selected"
+elif [[ $GRAPHICS = "a" ]]
+then
+  echo "AMD GPU Selected"
+elif [[ $GRAPHICS = "" ]]
+then
+  echo "GPU support will be added in future release"
+else
+  echo "ERROR:Invalid GPU selected"
+  exit_abnormal
+fi
+
+#Confirm install
+if [[ $GRAPHICS = "" ]]
+then
+while true; do
+    read -p "You have selected $OS with CPU only support, This will install Hashcat,OpenCL and Seclists. Do you wish to continue?" yn
+    case $yn in
+        [Yy]* ) echo "Continue with Install"; break;;
+        [Nn]* ) echo "Aborting" && exit_abnormal;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+else
+  while true; do
+      read -p "GPU Support will be added in a future release, do you want to continue with CPU support for $OS" yn
+      case $yn in
+          [Yy]* ) break;;
+          [Nn]* ) echo "Aborting" && exit_abnormal;;
+          * ) echo "Please answer yes or no.";;
+      esac
+  done
+fi
+
+#install commands based on OS
+if [[ $OS = "deb" ]] || [[ $OS = "ubuntu" ]]
+then
+  cd /root
+  mkdir ./hashes && mkdir ./hashcat && mkdir ./wordlists && mkdir ./opencl
+  cd hashcat
+  apt install opencl-headers -y
+  apt install pocl-opencl-icd -y
+  apt install cmake build-essential -y
+  apt install checkinstall git -y
+  git clone https://github.com/hashcat/hashcat.git
+  cd hashcat && git submodule update --init && make && make install
+  cd /root/wordlists
+  git clone https://github.com/danielmiessler/SecLists /root/wordlists/SecLists
+  tar -xf /root/wordlists/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz -C /root/wordlists/SecLists/
+  cd /root
+elif [[ $OS = "fedora" ]]
+then
+  cd /root
+  mkdir ./hashes && mkdir ./hashcat && mkdir ./wordlists && mkdir ./opencl
+  cd hashcat
+  dnf install opencl-headers -y
+  dnf install pocl -y
+  dnf install ocl-icd -y
+  dnf install cmake -y
+  dnf group install "C Development Tools and Libraries" -y
+  dnf install git -y
+  git clone https://github.com/hashcat/hashcat.git
+  cd hashcat && git submodule update --init && make && make install
+  cd /root/wordlists
+  git clone https://github.com/danielmiessler/SecLists /root/wordlists/SecLists
+  tar -xf /root/wordlists/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz -C /root/wordlists/SecLists/
+elif [[ $OS = "arch" ]]
+then
+  cd /root
+  mkdir ./hashes && mkdir ./hashcat && mkdir ./wordlists && mkdir ./opencl
+  cd hashcat
+  pacman -S opencl-headers --noconfirm
+  pacman -S pocl --noconfirm
+  pacman -S cmake --noconfirm
+  pacman -S git --noconfirm
+  git clone https://github.com/hashcat/hashcat.git
+  cd hashcat && git submodule update --init && make && make install
+  cd /root/wordlists
+  git clone https://github.com/danielmiessler/SecLists /root/wordlists/SecLists
+  tar -xf /root/wordlists/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz -C /root/wordlists/SecLists/
+else
+  echo "ERROR:Invalid OS Selected"
+  exit_abnormal
+fi
+
+#Confirmation Messahe
+border "Install Complete"
+
+
+exit 0
